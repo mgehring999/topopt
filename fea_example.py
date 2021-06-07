16b30661b3af1c63134a21fd041a6fd25ec67fa2
marc-win <marc.gehring@web.de>
2021-06-07 21:56:12 +0200

    fea with 2x2 mesh

--- fea_example.py
+++ fea_example.py
@@ -10,11 +10,11 @@ nodes, _, elements, loads = pre.readin(folder=folder)
 
 # assembly operator
 DME , IBC , neq = ass.DME(nodes, elements)
+nele = len(elements[:,0])
 
 # construct material array for topology optimiziation
 volfrac = 0.5
 nu = 0.3
-nele = len(elements[:,0])
 elem_E0 = np.ones((nele,1))*volfrac
 elem_nu = np.ones((nele,1))*nu
 mats = np.concatenate((elem_E0,elem_nu),axis=1)
@@ -27,8 +27,18 @@ RHSG = ass.loadasem(loads, IBC, neq)
 # solution
 UG = sol.static_sol(KG, RHSG)
 
+# calc compliance
+KGdense = KG.todense()
+r = np.matmul(KGdense,UG)
+r = r.T
+print("rechts",r)
+c = np.matmul(UG,r)
+print("compliance",c)
+
 # post processing
 UC = pos.complete_disp(IBC, nodes, UG)
+print(UC)
+print(UC.flatten())
 pos.fields_plot(elements, nodes, UC)
 
 # output

ae284b22362213bfc7cae0731af73212b60c9d74
marc-win <marc.gehring@web.de>
2021-06-07 08:38:37 +0200

    different material for each element

--- fea_example.py
+++ fea_example.py
@@ -3,12 +3,22 @@ import solidspy.solutil as sol
 import solidspy.postprocesor as pos
 import solidspy.preprocesor as pre
 import matplotlib.pyplot as plt
+import numpy as np
 
 folder = "./examples/mesh/"
-nodes, mats, elements, loads = pre.readin(folder=folder)
+nodes, _, elements, loads = pre.readin(folder=folder)
 
 # assembly operator
 DME , IBC , neq = ass.DME(nodes, elements)
+
+# construct material array for topology optimiziation
+volfrac = 0.5
+nu = 0.3
+nele = len(elements[:,0])
+elem_E0 = np.ones((nele,1))*volfrac
+elem_nu = np.ones((nele,1))*nu
+mats = np.concatenate((elem_E0,elem_nu),axis=1)
+print(mats)
 
 # System assembly
 KG = ass.assembler(elements, mats, nodes, neq, DME)

6e4e0cf103804b9a18e685b7fc55a7bcbc8d9449
marc <marc.gehring@web.de>
2021-07-01 08:40:19 +0200

    extract variables from model and plot them

--- topo.py
+++ topo.py
@@ -1,7 +1,18 @@
 from model import model
 from pyomo.opt import SolverFactory
+import solidspy.postprocesor as pos
+import matplotlib.pyplot as plt
+import numpy as np
 
-model.display()
 opt = SolverFactory("scip")
 result = opt.solve(model,tee=True)
 print(result)
+
+x = np.array([model.x[i].value for i in model.elems])
+u = np.array([model.u[i].value for i in model.eq])
+print(x)
+
+fig,axs = plt.subplots()
+axs.imshow(x.reshape((2,2)))
+
+plt.show()

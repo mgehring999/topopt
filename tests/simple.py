031346f093cb4c46fad1aeee93f8a803c6b05535
marc <marc.gehring@web.de>
2021-07-02 19:54:32 +0200

    wip: test decogo with simple model  first

--- tests/simple.py
+++ tests/simple.py
@@ -3,7 +3,7 @@ import pyomo.environ as pyo
 model = pyo.ConcreteModel()
 
 model.x = pyo.Var([1,2],domain=pyo.NonNegativeReals)
-model.OBJ = pyo.Objective(expr=model.x[1]**2+model.x[2]**2)
+model.OBJ = pyo.Objective(expr=model.x[1]**2+model.x[2]**2,sense=1)
 
 model.Constraint1 = pyo.Constraint(expr=model.x[1] + 2 * model.x[2] == 2)
 
--- tests/test_decogo.py
+++ tests/test_decogo.py
@@ -1,23 +1,19 @@
 from simple import model
 import sys, os
 
-# add Decogo source files to path
-# very important to do it before importing Decogo
-#path = r"C:\path\to\src
-#sys.path.insert(0, path)
-
 from decogo.solver.decogo import DecogoSolver
 
-# overwrite solver default settings and put the file in the working directory
-with open('decogo.set', 'w') as file:
-    file.write('strategy = OA\n')
-    file.write('decomp_estimate_var_bounds = True')
-    file.close()
+if __name__ == "__main__":
+    # overwrite solver default settings and put the file in the working directory
+    with open('decogo.set', 'w') as file:
+        file.write('strategy = OA\n')
+        file.write('decomp_estimate_var_bounds = True')
+        file.close()
 
-# create solver instance
-solver = DecogoSolver()
+    # create solver instance
+    solver = DecogoSolver()
 
-# solve the model
-solver.optimize(model)
+    # solve the model
+    solver.optimize(model)
 
-os.remove('decogo.set') # remove settings file
+    os.remove('decogo.set') # remove settings file

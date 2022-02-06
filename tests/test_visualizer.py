ef107c3dbf720e1d0adb2caf4f96e8a5a6d02f23
marc <marc.gehring@web.de>
2022-02-06 12:58:40 +0100

    resolved conflicts in tests

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -1,12 +1,12 @@
-from pdb import pm
 import numpy as np
 from topopt.topopt import Visualizer
-import glob, os
+import glob, os, pytest
+
+class PhysicalModelStub():
+    def __init__(self,nelem):
+        self.x = np.arange(nelem**2)
 
 def test_write_file_exist():
-    class PhysicalModelStub():
-        def __init__(self,nelem):
-            self.x = np.arange(nelem**2)
 
     pmodel = PhysicalModelStub(10)
 
@@ -24,9 +24,6 @@ def test_write_file_exist():
     assert vis.filename in glob.glob(vis.filename)
 
 def test_data_in_file():
-    class PhysicalModelStub():
-        def __init__(self,nelem):
-            self.x = np.arange(nelem**2)
     
     pmodel = PhysicalModelStub(10)
 
@@ -37,3 +34,15 @@ def test_data_in_file():
     os.remove(vis.filename)
     
     assert np.allclose(pmodel.x,test_data)
+
+@pytest.mark.parametrize("ndiv",range(1,100,10))
+def test_vis_data(ndiv):
+    class PhysicalModelStub():
+        def __init__(self,nelem):
+            self.x = np.arange(nelem**2)
+    pmodel = PhysicalModelStub(ndiv)
+    
+    vis = Visualizer(pmodel)
+    plot_data = vis.plot_result()
+    
+    assert list(pmodel.x) == list(plot_data.get_array().flatten())

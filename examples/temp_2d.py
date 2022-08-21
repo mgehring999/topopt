1844495697669f4125e2385162d5346adfb180bb
marc <marc.gehring@web.de>
2022-08-21 10:04:03 +0200

    Material
    - add thermal params for thermal fe model support

--- examples/temp_2d.py
+++ examples/temp_2d.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from topopt.topopt import StructuralOptim, Visualizer
-from topopt.physical import PhysicalModel, Material
-from topopt.mesh import Displacement, Heat, Mesh, Temperature
+import numpy as np
+
+from topopt.physical import PhysicalModel, Material 
+from topopt.mesh import Mesh, Temperature
+
 import matplotlib.pyplot as plt
 
-
 volfrac = 0.5
-ndiv = 10
+ndiv = 2
 
 mesh = Mesh()
 mesh.rect_mesh(ndiv)
@@ -17,8 +18,7 @@ temp_loads.add_by_plane([1,0],-1,100)
 temp_loads.add_by_plane([1,0],1,20)
 
 mat = Material()
-mat.set_structural_params(2.1e5,.3) # thermal conductivity, specific heat, density
+mat.set_thermal_params(0.05) # conductivity W/mmK
 
 bcs = [temp_loads]
 pmodel = PhysicalModel(mesh,mat,bcs)
-pmodel = PhysicalModel(mesh,temp_loads,mat)
